835fc9321a9547fcc58388a3fce3414c26cc02f4
ShahbozHusniddinov <sahbozhusniddinov84@gmail.com>
2022-06-28 10:29:51 +0500

    Shahboz

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import { About,Mani,Watch } from "./components/All"
 function From() {
   return (
     <>
+    <About/>
+    <Mani/>
+    <Watch/>
     </>
   )
 }
+export default From
---
+++ src/components/All.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <>
+        <div className='about-first'>
+            <div className='about-second'>
+                <h1>Uma's Paintings</h1>
+                <p>Check out our lead protagonist Umas paintings here. <br /> 
+                If you love them, get a hand painted one from her</p>
+            </div>
+        </div>
+    </>
+  )
+}
+export const Mani = () => {
+  return (
+    <>
+    <div className='main-all'>
+        <div className='mani-first'>
+            <div className='mani-second'>
+                <h1>50% of the money generated will be spent on coral rehabilitation. This is our pledge.</h1>
+            </div>
+            <div className='mani-first1'></div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+
+export const Watch = () => {
+  return (
+    <>
+    <div className='watch-first'>
+        <div className='watch-second'>
+            <h1>ABOUT UMA MANI</h1>
+            <p>A filmmaker's journey with Uma, a certified scuba diver, <br /> exploring the underwater world and the threat to coral.  
+            <br />
+            A homemaker from Tamil Nadu, who, through her paint, <br /> has been trying to draw public attention to the devastat <br /> effects of climate change on marine life and the coasta <br /> communities.
+            It is, in fact, her love for corals that inspire <br /> Uma to learn how to swim, dive and paint in her 50s</p>
+            <div className='btn'>
+                <button>Watch Video</button>
+            </div>
+        </div>
+        <div className='watch-img'>
+            <img src="./img/watch.png" alt="" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+
