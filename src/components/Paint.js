fd19f7d046e56ef14c24f9bd1945016e0462ce2c
KlipeLegend <suinbayevxumoyun20@gmail.com>
2022-06-30 10:09:22 +0500

    Xumoyun

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -4,11 +4,7 @@ import { Modal22 } from "./All"
 const Paint = () => {
   return (
     <div>
-<<<<<<< HEAD
     <Modal22/>
-=======
-      paint
->>>>>>> f015468921e1bcf8ea4798b35adc559622a33c79
     </div>
   )
 }
