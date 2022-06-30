db5bfdb5726db6e20a40182ac892d3b47796f3a3
KlipeLegend <suinbayevxumoyun20@gmail.com>
2022-06-30 10:30:00 +0500

    bahromjon

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { All } from './All'
 
 const About = () => {
   return (
-    <div> <All/></div>
+    <div> 
+
+    </div>
   )
 }
 
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { All, Modal22 } from './All'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+       <All/>
+         <Modal22/>
+    </div>
   )
 }
 
